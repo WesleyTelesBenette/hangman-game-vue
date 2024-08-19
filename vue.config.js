6072e88139f5cd578b132743221606d16902e2d3
Wesley T. Benette <weluonf@gmail.com>
2024-08-19 09:16:55 -0300

    :sparkles: feat: Creates styling patterns

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "~@/assets/scss/variables.scss";
-        `
-      }
-    }
-  }
+  transpileDependencies: true
 })
