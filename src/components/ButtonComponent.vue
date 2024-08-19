087717da48ad6011a781e4fa61c4add0f991f7b8
Wesley T. Benette <weluonf@gmail.com>
2024-08-19 13:55:08 -0300

    :sparkles: feat: Creates Button Component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,45 @@
+<template>
+	<input class="button click-soft" :type="type" :value="text"/>
+</template>
+
+<script>
+export default {
+	name: 'ButtonComponent',
+	props: {
+		type: {
+			type: String,
+			default: 'button',
+			validator: (value) => {
+				return ['submit', 'button'].includes(value);
+			}
+		},
+		text: {
+			type: String,
+			default: 'click'
+		}
+	}
+};
+</script>
+
+<style>
+.button {
+	width: 100%;
+	height: 35px;
+	background-color: var(--back-marked);
+	border: none;
+	border-radius: 10px;
+
+	color: var(--text-normal);
+
+	transition: background-color .1s, color .3s, font-weight .2s;
+
+	&:active {
+		opacity: 1;
+		background-color: transparent;
+		border: 2px solid var(--back-marked);
+
+		color: var(--text-marked);
+		font-weight: 800;
+	}
+}
+</style>
