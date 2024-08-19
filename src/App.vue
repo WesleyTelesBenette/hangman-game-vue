60c9607de17eaf5a46b31b9d9a19a4c612f2ebb5
Wesley T. Benette <weluonf@gmail.com>
2024-08-19 13:55:51 -0300

    :sparkles: feat: Creates Login Page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<LoginPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './pages/LoginPage.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		LoginPage
+	}
 }
 </script>
 
 <style>
+@import '~@/assets/scss/main.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin: 0;
+	padding: 0;
 }
 </style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,98 @@
+<template>
+	<div class='login-container'>
+		<div class='logo-container anim-size-pop'>
+			<img class='logo-image' src='~@/assets/logo.png'/>
+			<h2 class='logo-subtitle'>Venha 3nf0rc@r suas ideias com a gente!</h2>
+		</div>
+		<form class='login-inputs'>
+			<InputComponent idInput="email" label="Email" class="anim-size-pop" type='email' v-model='email' placeHolder='Digite seu e-mail...'/>
+			<InputComponent idInput="password" label="Senha"  class="anim-size-pop" type='password' v-model='password' placeHolder='Digite a senha...' />
+			<div class='login-buttons'>
+				<ButtonComponent class="anim-size-pop" @click="loginAccount" type='submit' text='Entrar' />
+				<ButtonComponent class="anim-size-pop" @click="createAccount" type='submit' text='Criar Conta'/>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+import InputComponent from '@/components/InputComponent.vue';
+import ButtonComponent from '@/components/ButtonComponent.vue';
+
+export default {
+	name: 'LoginPage',
+	components: { InputComponent, ButtonComponent },
+	data() {
+		return {
+			email: '',
+			password: ''
+		};
+	},
+	methods: {
+		createAccount() {
+
+		},
+		loginAccount() {
+
+		}
+	},
+};
+</script>
+
+<style>
+	.login-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 50px;
+
+		min-width: 100vw;
+		max-width: 100vw;
+		min-height: 100vh;
+		max-height: 100vh;
+		background-color: var(--back-dark);
+
+		.logo-container {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			margin-top: 10vh;
+			gap: 20px;
+
+			width: 100%;
+			
+			.logo-image {
+				user-select: none;
+				width: 30vw;
+			}
+
+			.logo-subtitle {
+				color: var(--text-dark);
+			}
+		}
+
+		.login-inputs {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			gap: 10px;
+
+			width: 35%;
+
+			.login-buttons {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				justify-content: space-around;
+				margin-top: 20px;
+
+				width: 100%;
+
+				.button {
+					width: 45%;
+				}
+			}
+		}
+	}
+</style>
