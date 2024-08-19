0d6cdfc84e216e04add07ab8ffa386384066c3e3
Wesley T. Benette <weluonf@gmail.com>
2024-08-19 13:50:53 -0300

    :sparkles: feat: Update InputComponent
    - Changes the position of the "eye"
    - Adds a label

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,17 +1,28 @@
 <template>
-<div class='input-container'>
-	<input required class='input-field' :placeholder='placeHolder' :type='getType' v-model='valueInput' />
-	<a v-if='this.type === "password"' @click='toogleEye()' class='input-eye click' type='button'>
-		<img v-show='eye' class="input-eye-image" src='~@/assets/eye.svg'/>
-		<img v-show='!eye' class="input-eye-image eye-off" src='~@/assets/eye-off.svg'/>
-	</a>
-</div>
+	<div class='input-container'>
+		<label v-if='label !== ""' :id="idInput" class="input-label"> {{ label }} </label>
+		<div class="input-field-container">
+			<input required :id="idInput" class='input-field' :placeholder='placeHolder' :type='getType' v-model='valueInput' />
+			<a v-if='this.type === "password"' @click='toogleEye()' class='input-eye click' type='button'>
+				<img v-show='eye' class="input-eye-image" src='~@/assets/eye.svg'/>
+				<img v-show='!eye' class="input-eye-image eye-off" src='~@/assets/eye-off.svg'/>
+			</a>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
 	name: 'InputComponent',
 	props: {
+		idInput: {
+			type: String,
+			default: ''
+		},
+		label: {
+			type: String,
+			default: ''
+		},
 		placeHolder: {
 			type: String,
 			default: ''
@@ -53,45 +64,61 @@ export default {
 </script>
 
 <style>
+.input-label {
+	display: flex;
+	padding-left: 10px;
+
+	width: 100%;
+}
 .input-container {
 	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	max-width: 100%;
-	min-width: 100%;
-	height: 3em;
+	flex-direction: column;
+	width: 100%;
+	gap: 3px;
 
-	.input-field {
-		box-sizing: border-box;
-		padding: 0px 10px;
+	.input-field-container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		max-width: 100%;
+		min-width: 100%;
+		height: 3em;
 
-		width: 100%;
-		min-height: 100%;
-		max-height: 100%;
-		background-color: transparent;
-		border: 2px solid var(--border-normal);
-		border-radius: 20px;
-
-		color: var(--text-normal);
-
-		&:focus {
-			outline: none;
-			box-shadow: 0 0 10px var(--border-shadow);
-			border-color: var(--border-marked);
+		.input-field {
+			box-sizing: border-box;
+			padding: 0px 20px;
+			padding-right: 40px;
+	
+			min-width: 100%;
+			min-height: 100%;
+			max-height: 100%;
+			background-color: transparent;
+			border: 2px solid var(--border-normal);
+			border-radius: 20px;
+	
+			color: var(--text-normal);
+	
+			&:focus {
+				outline: none;
+				box-shadow: 0 0 10px var(--border-shadow);
+				border-color: var(--border-marked);
+			}
+	
+			transition: box-shadow .4s, border-color .3s;
 		}
-
-		transition: box-shadow .4s, border-color .3s;
 	}
 
 	.input-eye {
 		display: flex;
 		justify-content: center;
 		align-items: center;
+
+		transform: translateX(-50px);
 		height: 100%;
 
 		.input-eye-image {
 			margin-left: 10px;
-			width: 30px;
+			width: 25px;
 
 			&.eye-off {
 				opacity: .5;
