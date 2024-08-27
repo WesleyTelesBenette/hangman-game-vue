a0c81084c21533c0f006aeda93fae3a0854b8c38
Wesley T. Benette <weluonf@gmail.com>
2024-08-27 11:37:43 -0300

    :sparkles: feat(CardComponent): Adds action buttons

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,6 +1,14 @@
 <template>
-	<div @click='clickOnCard' class='card-container click anim-size-pop-delay'>
-		{{ content }}
+	<div class='card-container anim-size-pop-delay'>
+		<div class='card-element'>
+			{{ content }}
+		</div>
+		<div class='card-buttons'>
+			<a @click='buttonsAction("play")' class='click-hard'><img class='card-button-image' src='~@/assets/play.svg'/></a>
+			<a @click='buttonsAction("copy")' class='click-hard'><img class='card-button-image' src='~@/assets/copy.svg'/></a>
+			<a @click='buttonsAction("edit")' class='click-hard'><img class='card-button-image' src='~@/assets/edit.svg'/></a>
+			<a @click='buttonsAction("delete")' class='click-hard'><img class='card-button-image' src='~@/assets/delete.svg'/></a>
+		</div>
 	</div>
 </template>
 
@@ -8,14 +16,23 @@
 export default {
 	name: 'CardComponent',
 	props: {
+		idCard: {
+			type: Number
+		},
 		content: {
 			type: String,
 			default: ''
 		}
 	},
 	methods: {
-		clickOnCard() {
-			this.$emit('click-card', this.content);
+		buttonsAction(action) {
+			const validAction = ['play', 'copy', 'edit', 'delete'].includes(action);
+
+			if (validAction) {
+				this.$emit('buttons-action', this.idCard, action);
+				return;
+			}
+			throw Error('Origin: CardComponent->buttonsAction()');
 		}
 	}
 }
@@ -23,17 +40,43 @@ export default {
 
 <style>
 .card-container {
-	display: inline-block;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 	user-select: none;
-	padding: 20px 15px;
+	overflow: hidden;
+	padding: 15px 15px;
 
 	width: 100%;
-	background-color: var(--back-light);
+	height: 120px;
+	border: 2px solid var(--back-light);
 	border-radius: 8px;
 
-	font-size: 1.2em;
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
+	.card-element {
+		padding: 10px;
+
+		background-color: var(--back-normal);
+		border-radius: 10px;
+
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+
+	.card-buttons {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+		align-items: center;
+
+		max-width: 100%;
+		height: 20px;
+
+		.card-button-image {
+			opacity: .9;
+			max-height: 30px;
+		}
+	}
 }
+
 </style>
